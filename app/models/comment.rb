5b16c4a39f659876651fc7050c66ce6646a3cc33
sutantoc <165302919+sutantoc@users.noreply.github.com>
2024-05-20 19:12:15 +0000

    comment and follow requests

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -19,15 +19,19 @@ class Comment < ApplicationRecord
 
   # Comment#commenter: returns a row from the users table associated to this comment by the author_id column
 
+  belongs_to( :commenter, class_name: "User", foreign_key: "author_id")
+
   # Comment#photo: returns a row from the photos table associated to this comment by the photo_id column
 
-  def commenter
-    my_author_id = self.author_id
+  belongs_to( :photo, class_name: "Photo", foreign_key: "photo_id")
 
-    matching_users = User.where({ :id => my_author_id })
+  #def commenter
+    #my_author_id = self.author_id
 
-    the_user = matching_users.at(0)
+    #matching_users = User.where({ :id => my_author_id })
 
-    return the_user
-  end
+    #the_user = matching_users.at(0)
+
+    #return the_user
+  #end
 end
